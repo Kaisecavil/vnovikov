53d813971737408d587325516c210b14453c157e
Kaisecavil <combatzara5094@mail.ru>
2022-08-09 22:47:27 +0300

    Tic tac toe v2(working version)

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,35 +1,66 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
 namespace ConsoleApp1
 {
     public class Program
     {
+       
 
         static void Main(string[] args)
         {
-            int rows = 3;
-            int columns = 3;
-            string fPlayerName = InputName();
-            string sPlayerName = InputName();
-            char[,] field = new char[rows, columns];
+            GameField gameField = new GameField(3, 3, 2);
+            List<string> players = new List<string> { InputName(), InputName() }; //лист тут необходим ,так как много где используется вдинамическиъ сообщениях типо: Console.Write($"Player №{turn%2+1} ({players[turn % 2]}) Input two numbers [1-3]:");
 
-            int turnCounter = 0;
-            SetStartField(rows, columns, field);
-            while (CheckVictory(field, turnCounter) == 0)
+            int x = -1; //переменные для запоминания координат и состояния игры
+            int y = -1;
+            int res = 0;
+            while (gameField.CheckVictory(out res) == 0)//игра идет до того момента пока метод определения победы не выдаст результат отличный от нуля или пока не закочится поле
             {
-                DisplayField(rows, columns, field);
-                Turn(ref field,ref turnCounter);
+                while(true)
+                {
+                    
+                    try // запрашиваем на ввод необходимую информацию и если она не подходит по тем или иным причинам запрашиваем ещё раз
+                    {
+                        ParseTurnInfo(AskForTurnInfo(gameField.turnCounter, players), out x, out y);
+                        gameField.MakeTurn(x, y);
 
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                        continue;
+                    }
+                    break;
+                }
+                gameField.DisplayField();
             }
+            if (res == -1)
+            {
+                Console.WriteLine("DRAW");
+            }
+            else
+            {
+                Console.WriteLine($"Player №{res} ({players[res-1]}) won");
+            }
+            Console.WriteLine("Do you wanna to repeat? type 1 or 2\n1.yes\n2.no");
+            if (Console.Read() == '1')
+            {
+                Main(args); //не уверен насчёт этого способа повтора игры , при нём во первых почему-то вылетает две просьбы ввести пользователя ник . и ещё возможно переполнение стека вызова в теории
+            }
+            
         }
 
-        public static int CheckVictory(char[,] fld, int turn)
+        public static string AskForTurnInfo(int turn, List<string> players) //Метод запрашиваюший у нужного игрока строку на ввод
         {
-            return 0;
+            Console.Write($"Player №{turn%2+1} ({players[turn % 2]}) Input two numbers [1-3]:");
+            return Console.ReadLine();
         }
 
-        public static void ParseTurn(string turnStr, out int x, out int y)
+        
+
+        public static void ParseTurnInfo(string turnStr, out int x, out int y)//Метод проверяюший формат ввода и выуживающий из него две координаты
         {
             Regex regex = new Regex(@"^\d \d$");
             bool isValid = regex.IsMatch(turnStr);
@@ -42,6 +73,7 @@ namespace ConsoleApp1
             }
             else
             {
+                
                 x = -1;
                 y = -1;
 
@@ -49,101 +81,24 @@ namespace ConsoleApp1
 
         }
 
-        public static string InputName()
+        public static string InputName()//Метод для ввода и валидации имени игрока
         {
             string name = "";
-            while (name.Length >= 25 || name.Length == 0)
+            while (name.Length >= 25 || name.Length==0)
             {
+                if (name.Length >= 25)
+                {
+                    Console.WriteLine("invalid name length");
+                }
                 Console.WriteLine("Input Player Name [1-25] characters:");
                 name = Console.ReadLine();
             }
             return name;
         }
 
-        public static void Turn(ref char[,] fld,ref int numOfTurn)
-        {
-            Random random = new Random();
-            if (numOfTurn % 2 == 0)
-            {
-                Console.Write("First Player turn, input two numbers [1-3]: ");
-            }
-            else
-            {
-                Console.Write("Second turn, input two numbers [1-3]: ");
-            }
-            string turn;
-            int x = -1;
-            int y = -1;
-            for (int i = 0; i < 3; i++)
-            {
-                turn = Console.ReadLine();
-                ParseTurn(turn, out x, out y);
-                if (!IsValidCoords(x, y, ref fld))
-                {
-                    Console.WriteLine($"Wrong cords, try again {i + 1} attempt out of 3");
-                }
-                else
-                {
-                    break;
-                }
-                if (i == 2)
-                {
-                    Console.WriteLine("3 of 3");
-                    numOfTurn++;
-                    return;
-                }
+        
 
-            }
-            if (numOfTurn % 2 == 0)
-            {
-                PutMark(ref fld, x, y, 'X');
-            }
-            else
-            {
-                PutMark(ref fld, x, y, 'O');
-            }
-            numOfTurn++;
-
-        }
-
-        public static void PutMark(ref char[,] fld, int x, int y, char mark)
-        {
-            fld[x - 1, y - 1] = mark;
-        }
-
-        public static void SetStartField(int Rows, int Columns, char[,] Field)
-        {
-            for (int i = 0; i < Rows; i++)
-                for (int j = 0; j < Columns; j++)
-                {
-                    Field[i, j] = '.';
-                }
-        }
-
-        public static void DisplayField(int Rows, int Columns, char[,] Field)
-        {
-            for (int i = 0; i < Rows; i++)
-            {
-                //Console.WriteLine();
-                for (int j = 0; j < Columns; j++)
-                {
-                    if (j != 1)
-                        Console.Write($"{Field[i, j]}");
-                    else
-                        Console.Write($"|{Field[i, j]}|");
-                }
-
-                Console.WriteLine();
-                if (i != 2)
-                    Console.WriteLine("-----");
-            }
-            Console.ReadKey();
-        }
-
-        public static bool IsValidCoords(int x, int y, ref char[,] fld)
-        {
-            return (x >= 1 && y >= 1 && x < 4 && y < 4) && fld[x - 1, y - 1] == '.';
-        }
+        
 
 
     }
